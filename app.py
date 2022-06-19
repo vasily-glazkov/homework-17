65549b85e0e42fea5755ec4b0bbdae6d4ce5c16f
vasily-glazkov <vglazkoff@gmail.com>
2022-06-19 16:14:44 +0300

    homework is ready to be sent

--- app.py
+++ app.py
@@ -9,6 +9,8 @@ app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///test.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
+api = Api(app)
+movies_ns = api.namespace('movies')
 
 
 class Movie(db.Model):
@@ -24,6 +26,22 @@ class Movie(db.Model):
     director_id = db.Column(db.Integer, db.ForeignKey("director.id"))
     director = db.relationship("Director")
 
+
+class MovieSchema(Schema):
+    id = fields.Int()
+    title = fields.Str()
+    description = fields.Str()
+    trailer = fields.Str()
+    year = fields.Int()
+    rating = fields.Float()
+    genre_id = fields.Int()
+    director_id = fields.Int()
+
+
+movie_schema = MovieSchema()
+movies_schema = MovieSchema(many=True)
+
+
 class Director(db.Model):
     __tablename__ = 'director'
     id = db.Column(db.Integer, primary_key=True)
@@ -35,6 +53,31 @@ class Genre(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(255))
 
+
+@movies_ns.route('/')
+class MoviesView(Resource):
+    def get(self):
+        movies = db.session.query(Movie)
+        director_id = request.args.get('director_id')
+        genre_id = request.args.get('genre_id')
+
+        if director_id is not None:
+            movies = movies.filter(Movie.director_id == director_id)
+        if genre_id is not None:
+            movies = movies.filter(Movie.genre_id == genre_id)
+        movies = movies.all()
+
+        return movies_schema.dump(movies), 200
+
+
+@movies_ns.route('<int:mid>')
+class MovieView(Resource):
+    def get(self, mid):
+        try:
+            movie = db.session.query(Movie).filter(Movie.id == mid).one()
+            return movie_schema.dump(movie), 200
+        except Exception as e:
+            return str(e), 404
 
 
 if __name__ == '__main__':
